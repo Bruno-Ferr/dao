8b3111a8b6f3557eb22246aa11a2a9c5662e4879
Bruno Ferreira <fbruno233@gmail.com>
2024-07-05 12:01:36 -0300

    add URI to ERC721 contract

--- contracts/Teste.sol
+++ contracts/Teste.sol
@@ -2,15 +2,16 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract MyContract is ERC721 {
+contract MyContract is ERC721URIStorage {
   address owner;
-
+  uint256 public tokenCounter;
   constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
     // Inicialização do contrato
     owner = msg.sender;
+    tokenCounter = 0;
   }
 
   modifier onlyOwner {
@@ -18,8 +19,13 @@ contract MyContract is ERC721 {
     _;
   }
 
-  function mint(address _to, uint256 _tokenId) public onlyOwner {
-    _mint(_to, _tokenId);
+  function createNft(address _to, string memory _tokenURI, uint groupNumber) public onlyOwner returns (uint256) {
+    uint256 newItemId = tokenCounter;
+    _safeMint(_to, newItemId);
+    _setTokenURI(newItemId, _tokenURI);
+    tokenCounter = tokenCounter + 1;
+    daoGroups[groupNumber].nftHolders.push(_to);
+    return newItemId;
 }
 
   struct Proposal {
@@ -39,7 +45,7 @@ contract MyContract is ERC721 {
   mapping(uint => DAOGroup) public daoGroups;
   mapping(uint256 => Proposal) proposals;
 
-  function registerDAOGroup(uint groupId, string memory groupName) public {
+  function registerDAOGroup(uint groupId, string memory groupName) public onlyOwner {
     address[] memory nftHolders;
     uint[] memory daoProposals;
     daoGroups[groupId] = DAOGroup(groupName, nftHolders, daoProposals);
